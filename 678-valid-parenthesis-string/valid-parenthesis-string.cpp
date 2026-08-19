4adfc0fbb74da157b860ab365802e842b889d3ad
VanshikaSijwali <sijwalivanshika@gmail.com>
2026-08-19 22:15:35 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (93.41%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -1,43 +1,42 @@
 class Solution {
 public:
-int solve(string s,int count,int i,vector<vector<int>>&dp)
-{
-    if(count<0)
-    {return 0;}
-    if(dp[i][count]!=-1)
-    {return dp[i][count];}
-
-    if(i==s.size())
-    {
-        if(count==0)
-        {dp[i][count]=1;}
-        else
-        {dp[i][count]=0;}
-    }
-    else
-    {
-        if(s[i]=='(')
-        {
-            dp[i][count]=solve(s,count+1,i+1,dp);
-
-        }
-        else if(s[i]==')')
-        {
-            dp[i][count]=solve(s,count-1,i+1,dp);
-        }
-        else
-        {
-           dp[i][count]=solve(s,count+1,i+1,dp)||solve(s,count,i+1,dp)||solve(s,count-1,i+1,dp);
-        }
-    }
-    return dp[i][count];
-}
-    
     bool checkValidString(string s) {
-        
-        vector<vector<int>>dp(s.size()+1,vector<int>(s.size()+1,-1));
-        return solve(s,0,0,dp)==1;
+        int  min=0,max=0;
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]=='(')
+            {min++;
+            max++;}
+            else if(s[i]==')')
+            {
+                if(min==0)
+                {
+                    if(max==0)
+                    {return false;}
+                    else
+                    {max--;}
+                    
+                }
+                else
+                {
+                    min--;
+                    max--;
+                }
+            }
+            else
+            {
+                if(min!=0)
+                
+              {min--;}
+                max++;
+            }
 
+
+
+
+
+        }
+        return min==0;
         
     }
 };
