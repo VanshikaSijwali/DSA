08bd70928c8e85276739ecf53d400bd3e7c0feed
VanshikaSijwali <sijwalivanshika@gmail.com>
2026-09-25 01:55:14 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (88.63%) - LeetSync

--- 500-keyboard-row/keyboard-row.cpp
+++ 500-keyboard-row/keyboard-row.cpp
@@ -1,29 +1,38 @@
 class Solution {
 public:
     vector<string> findWords(vector<string>& words) {
-        unordered_map <char,int>mp={
-      {'q',1},{'Q',1},{'w',1},{'W',1},{'e',1},{'E',1},{'r',1},{'R',1},{'T',1},{'t',1},{'y',1},{'Y',1},{'U',1},{'u',1},{'i',1},{'I',1},{'o',1},{'O',1},{'P',1},{'p',1},
-      {'a',2},{'A',2},{'s',2},{'S',2},{'d',2},{'D',2},{'F',2},{'f',2},{'g',2},{'G',2},
-      {'h',2},{'H',2},{'j',2},{'J',2},{'k',2},{'K',2},{'l',2},{'L',2},
-      {'z',3},{'Z',3},{'x',3},{'X',3},{'c',3},{'C',3},{'v',3},{'V',3},{'B',3},{'b',3},
-      {'n',3},{'N',3},{'m',3},{'M',3}
+        string row1="qwertyuiop";
+        string row2="asdfghjkl";
+        string row3="zxcvbnm";
 
-
-        };
         vector<string>ans;
-        for(int i=0;i<words.size();i++)
-        {
-            int num=mp[words[i][0]];
-            bool flag=true;
-            for(int j=1;j<words[i].size();j++)
-            {
-                if(mp[words[i][j]]==num)
-                {continue;}
-                else
-                {flag=false;}
+        for(string word:words){
+            string row;
+            char first=tolower(word[0]);
+
+            if(row1.find(first) != string::npos){
+                row=row1;
             }
-            if(flag)
-            {ans.push_back(words[i]);}
+            else if(row2.find(first) != string::npos){
+                row=row2;
+            }
+            else{
+                row=row3;
+            }
+
+            bool valid=true;
+
+            for (char ch : word) {
+                ch = tolower(ch);
+
+                if (row.find(ch) == string::npos) {
+                    valid = false;
+                    break;
+                }
+            }
+            if (valid) {
+                ans.push_back(word);
+            }
         }
         return ans;
     }
